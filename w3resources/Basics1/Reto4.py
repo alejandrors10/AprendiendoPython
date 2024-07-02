d0356155ca8b83767f59b58179d77da78015b975
alejandroruiz10 <123484881+nutriamarciana@users.noreply.github.com>
2024-07-02 18:25:36 +0200

    Reinicio

---
+++ w3resources/Basics1/Reto4.py
@@ -0,0 +1,12 @@
+# 4. Write a Python program that calculates the area of a circle based on the radius entered by the user.
+# Sample Output :
+# r = 1.1
+# Area = 3.8013271108436504
+
+from math import pi
+
+r=float(input("Introduce el radio del círculo: "))
+
+area = pi * r **2
+
+print(area)
