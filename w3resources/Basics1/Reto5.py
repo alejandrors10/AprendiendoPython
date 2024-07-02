c05305558cb13ff84a4d113bb5beaa9a9779207c
alejandroruiz10 <123484881+nutriamarciana@users.noreply.github.com>
2024-07-02 18:51:33 +0200

    Ejercicios 1-5

---
+++ w3resources/Basics1/Reto5.py
@@ -0,0 +1,8 @@
+# 5. Write a Python program that accepts
+# the user's first and last name and prints 
+# them in reverse order with a space between them.
+
+a = input("Introduce el nombre: ")
+b = input("Introduce el primer apellido: ")
+
+print(b + " " + a)
